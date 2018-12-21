1273a8026a0bf06cae432d580e14bb6c7745dbf4
deby225 <doudoudeby@gmail.com>
2018-12-21 03:36:15 +0000

    nettoyage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProfilComponent } from './driver/profil/profil.component';
 
 import {RouterModule, Routes} from '@angular/router';
-import { ProfilclientComponent } from './client/profilclient/profilclient.component';
-import { LoginclientComponent } from './client/loginclient/loginclient.component';
-import { RegisterclientComponent } from './client/registerclient/registerclient.component';
-import { RegisterComponent } from './driver/register/register.component';
-import { LoginComponent } from './driver/login/login.component';
+
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
 import {
@@ -36,39 +31,7 @@ import { HeadersComponent } from './pages/headers/headers.component';
 
 const routerconfig: Routes = [
   {path: '' , component: HomeComponent },
-  {path: 'home' , component: HomeComponent },
-  {path: 'driver' , component: ProfilComponent,
-    children : [
-      {
-        path: 'profil',
-        component: ProfilComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-    ]
-
-
-  },
-  // {path: 'client' , component: ProfilclientComponent,
-  //   children: [
-  //     {
-  //       path: 'login',
-  //       component: LoginclientComponent
-  //     },
-  //     {
-  //       path: 'register',
-  //       component: RegisterclientComponent
-  //     },
-  //   ]
-  //
-  //
-  // },
+  {path: 'home' , component: HomeComponent }
 ];
 
 
@@ -76,12 +39,6 @@ const routerconfig: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    ProfilComponent,
-    ProfilclientComponent,
-    LoginclientComponent,
-    RegisterclientComponent,
-    RegisterComponent,
-    LoginComponent,
     HeadersComponent
   ],
   imports: [
